e9c9b9ff962102207199ca37e3fabaee7c924478
alexey <alexey@toklovich.com>
2017-11-20 22:26:23 +0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.js-mediacatalog-item-carousel').slick({
+        arrows: false,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        touchThreshold: 10
+    });
+});
